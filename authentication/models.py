d5a1e7391aa291d96cbec9189aaf1d52da869535
Niran S Narayanan <nirans2002@gmail.com>
2022-07-20 14:04:15 +0530

    auth

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+# from django.utils.translation import ugettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email,password,**extra_fields):
+        if not email:
+            raise ValueError('email cannot be empty')
+        
+        email = self.normalize_email(email)
+        # create new_user
+        new_user = self.model(email=email,*extra_fields)
+        # set default password for new user 
+        new_user.set_password(password)
+        new_user.save()
+
+        return new_user
+
+
+    def create_superuser(self,email,password,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active',True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        if extra_fields.get('is_active') is not True:
+            raise ValueError('Superuser must have is_active=True')
+
+        return self.create_user(email,password,**extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=50 , unique=True)
+    email = models.EmailField(max_length=255 , unique=True)
+    phone_no = PhoneNumberField(null=False,unique=True)
+
+    USERNAME_FIELD = 'email'   # username field is email
+
+    REQUIRED_FIELDS = ['username','phone_no']
+
+    def __str__(self) -> str:
+        return f"<user  {self.email}>"
--- pizza_server/settings.py
+++ pizza_server/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'phonenumber_field',
     
     'authentication.apps.AuthenticationConfig',
     'orders.apps.OrdersConfig',
@@ -127,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'authentication.CustomUser'
