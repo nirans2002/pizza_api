1579ba981ef1ca164cc1f99e075c01708296698b
Niran S Narayanan <nirans2002@gmail.com>
2022-07-20 13:25:28 +0530

    browsable api

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HelloAuthView.as_view(), name='auth'),
+    path('', views.HelloOrderView.as_view(), name='orders'),
 ]
